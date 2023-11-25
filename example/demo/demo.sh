5c55eedb49acd9a2989bb7aceec9877f38705782
ZhijieXiong <18800118477@163.com>
2023-11-25 11:09:03 +0800

    update

---
+++ example/demo/demo.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+domain=0
+
+{
+  echo "hello 0"
+} >> /Users/dream/Desktop/code/projects/dlkt/example/demo/domain_"${domain}".txt
+
+domain=1
+
+{
+  echo "hello 1"
+} >> /Users/dream/Desktop/code/projects/dlkt/example/demo/domain_"${domain}".txt

adb02ef2aae596ca3481431a75c2fd5581440b5f
ZhijieXiong <18800118477@163.com>
2024-01-23 15:03:13 +0800

    update

---
+++ example/script_local/dkt/our_setting_assist2009_save.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+{
+  dataset_name="assist2009"
+  data_type="only_question"
+
+  for fold in 0 1 2 3 4
+  do
+    echo -e "fold: ${fold}"
+    python F:/code/myProjects/dlkt/example/train/dkt.py \
+      --setting_name "our_setting" --dataset_name "${dataset_name}" --data_type "${data_type}" \
+      --train_file_name "${dataset_name}_train_fold_${fold}.txt" --valid_file_name "${dataset_name}_valid_fold_${fold}.txt" --test_file_name "${dataset_name}_test_fold_${fold}.txt" \
+      --optimizer_type adam --weight_decay 0.0001 --momentum 0.9 \
+      --train_strategy valid_test --num_epoch 200 \
+      --use_early_stop True --epoch_early_stop 10 --use_last_average False --epoch_last_average 5 \
+      --main_metric AUC --use_multi_metrics False \
+      --learning_rate 0.001 --enable_lr_schedule False --lr_schedule_type MultiStepLR --lr_schedule_step 10 --lr_schedule_milestones "[5]" --lr_schedule_gamma 0.5 \
+      --train_batch_size 64 --evaluate_batch_size 128 \
+      --enable_clip_grad False --grad_clipped 10.0 \
+      --use_concept True --num_concept 123 --num_question 17751 \
+      --dim_emb 256 --dim_latent 256 --rnn_type gru --num_rnn_layer 1 --dropout 0.3 --num_predict_layer 2 --dim_predict_mid 256 --activate_type sigmoid \
+      --save_model True --seed 0
+  done
+} >> F:/code/myProjects/dlkt/example/result_local/dkt_our_setting_assist2009_save.txt
--- example/script_local/save.sh
+++ example/script_local/save.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-bash F:/code/myProjects/dlkt/example/script_local/akt/our_setting_assist2009_save.sh
-bash F:/code/myProjects/dlkt/example/script_local/akt/our_setting_assist2017_save.sh
-bash F:/code/myProjects/dlkt/example/script_local/akt/our_setting_edi2020-task34_save.sh
-bash F:/code/myProjects/dlkt/example/script_local/akt/our_setting_ednet-kt1_save.sh
+bash F:/code/myProjects/dlkt/example/script_local/dkt/our_setting_algebra2005_save.sh
+bash F:/code/myProjects/dlkt/example/script_local/dkt/our_setting_assist2009_save.sh
+bash F:/code/myProjects/dlkt/example/script_local/dkt/our_setting_assist2017_save.sh
+bash F:/code/myProjects/dlkt/example/script_local/dkt/our_setting_bridge2algebra2006_save.sh
+bash F:/code/myProjects/dlkt/example/script_local/dkt/our_setting_edi2020-task34_save.sh
+bash F:/code/myProjects/dlkt/example/script_local/dkt/our_setting_ednet-kt1_save.sh
 
 
 

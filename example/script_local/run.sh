ad2f4fe63a32c1ad28d77845418ad95fbe5c8c30
ZhijieXiong <18800118477@163.com>
2024-02-20 09:52:05 +0800

    update

--- example/script_local/run.sh
+++ example/script_local/run.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-bash F:/code/myProjects/dlkt/example/script_local/qdkt_ME-ADA/our_setting_assist2009_save.sh
-bash F:/code/myProjects/dlkt/example/script_local/qdkt_ME-ADA/our_setting_assist2017_save.sh
-bash F:/code/myProjects/dlkt/example/script_local/qdkt_ME-ADA/our_setting_algebra2005_save.sh
+bash F:/code/myProjects/dlkt/example/script_local/akt_ME-ADA/our_setting_assist2009_save.sh
+bash F:/code/myProjects/dlkt/example/script_local/qdkt_ME-ADA/our_setting_bridge2algebra2006_save.sh
+bash F:/code/myProjects/dlkt/example/script_local/qdkt_ME-ADA/our_setting_edi2020-task34_save.sh
+

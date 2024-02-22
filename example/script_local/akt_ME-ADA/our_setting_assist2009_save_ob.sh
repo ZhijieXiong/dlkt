a31239e8d6861bb9e6606e92fca7b5c03dc579d2
ZhijieXiong <18800118477@163.com>
2024-02-22 17:05:24 +0800

    update

---
+++ example/script_local/akt_ME-ADA/our_setting_assist2009_save_ob.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+
+{
+  dataset_name="assist2009"
+  data_type="only_question"
+
+  for fold in 0 1 2 3 4
+  do
+    echo -e "fold: ${fold}"
+    python F:/code/myProjects/dlkt/example/train/akt_max_entropy_aug.py \
+      --setting_name "our_setting" --dataset_name "${dataset_name}" --data_type "${data_type}" \
+      --train_file_name "${dataset_name}_train_fold_${fold}.txt" --valid_file_name "${dataset_name}_valid_fold_${fold}.txt" --test_file_name "${dataset_name}_test_fold_${fold}.txt" \
+      --optimizer_type "adam" --weight_decay 0 --momentum 0.9 \
+      --train_strategy "valid_test" --num_epoch 200 \
+      --use_early_stop True --epoch_early_stop 10 --use_last_average False --epoch_last_average 5 \
+      --main_metric "AUC" --use_multi_metrics False \
+      --learning_rate 0.0001 --enable_lr_schedule False --lr_schedule_type "MultiStepLR" --lr_schedule_step 10 --lr_schedule_milestones "[5, 10]" --lr_schedule_gamma 0.5 \
+      --train_batch_size 24 --evaluate_batch_size 128 \
+      --enable_clip_grad True --grad_clipped 10.0 \
+      --num_concept 123 --num_question 17751 \
+      --dim_model 256 --key_query_same True --num_head 8 --num_block 2 --dim_ff 256 --dim_final_fc 512 --dropout 0.1 \
+      --separate_qa False --seq_representation "encoder_output" --weight_rasch_loss 0.00001 \
+      --use_warm_up True --epoch_warm_up 1 \
+      --epoch_interval_generate 1 --epoch_generate 200 --weight_adv_pred_loss 0.5 --loop_adv 3 --adv_learning_rate 1 --eta 20 --gamma 20 \
+      --save_model False --debug_mode False --seed 0
+  done
+
+  for fold in 0 1 2 3 4
+  do
+    echo -e "fold: ${fold}"
+    python F:/code/myProjects/dlkt/example/train/akt_max_entropy_aug.py \
+      --setting_name "our_setting" --dataset_name "${dataset_name}" --data_type "${data_type}" \
+      --train_file_name "${dataset_name}_train_fold_${fold}.txt" --valid_file_name "${dataset_name}_valid_fold_${fold}.txt" --test_file_name "${dataset_name}_test_fold_${fold}.txt" \
+      --optimizer_type "adam" --weight_decay 0 --momentum 0.9 \
+      --train_strategy "valid_test" --num_epoch 200 \
+      --use_early_stop True --epoch_early_stop 10 --use_last_average False --epoch_last_average 5 \
+      --main_metric "AUC" --use_multi_metrics False \
+      --learning_rate 0.0001 --enable_lr_schedule False --lr_schedule_type "MultiStepLR" --lr_schedule_step 10 --lr_schedule_milestones "[5, 10]" --lr_schedule_gamma 0.5 \
+      --train_batch_size 24 --evaluate_batch_size 128 \
+      --enable_clip_grad True --grad_clipped 10.0 \
+      --num_concept 123 --num_question 17751 \
+      --dim_model 256 --key_query_same True --num_head 8 --num_block 2 --dim_ff 256 --dim_final_fc 512 --dropout 0.1 \
+      --separate_qa False --seq_representation "encoder_output" --weight_rasch_loss 0.00001 \
+      --use_warm_up True --epoch_warm_up 1 \
+      --epoch_interval_generate 2 --epoch_generate 200 --weight_adv_pred_loss 0.5 --loop_adv 3 --adv_learning_rate 1 --eta 20 --gamma 20 \
+      --save_model False --debug_mode False --seed 0
+  done
+
+  for fold in 0 1 2 3 4
+  do
+    echo -e "fold: ${fold}"
+    python F:/code/myProjects/dlkt/example/train/akt_max_entropy_aug.py \
+      --setting_name "our_setting" --dataset_name "${dataset_name}" --data_type "${data_type}" \
+      --train_file_name "${dataset_name}_train_fold_${fold}.txt" --valid_file_name "${dataset_name}_valid_fold_${fold}.txt" --test_file_name "${dataset_name}_test_fold_${fold}.txt" \
+      --optimizer_type "adam" --weight_decay 0 --momentum 0.9 \
+      --train_strategy "valid_test" --num_epoch 200 \
+      --use_early_stop True --epoch_early_stop 10 --use_last_average False --epoch_last_average 5 \
+      --main_metric "AUC" --use_multi_metrics False \
+      --learning_rate 0.0001 --enable_lr_schedule False --lr_schedule_type "MultiStepLR" --lr_schedule_step 10 --lr_schedule_milestones "[5, 10]" --lr_schedule_gamma 0.5 \
+      --train_batch_size 24 --evaluate_batch_size 128 \
+      --enable_clip_grad True --grad_clipped 10.0 \
+      --num_concept 123 --num_question 17751 \
+      --dim_model 256 --key_query_same True --num_head 8 --num_block 2 --dim_ff 256 --dim_final_fc 512 --dropout 0.1 \
+      --separate_qa False --seq_representation "encoder_output" --weight_rasch_loss 0.00001 \
+      --use_warm_up True --epoch_warm_up 1 \
+      --epoch_interval_generate 3 --epoch_generate 200 --weight_adv_pred_loss 0.5 --loop_adv 3 --adv_learning_rate 1 --eta 20 --gamma 20 \
+      --save_model False --debug_mode False --seed 0
+  done
+
+} >> F:/code/myProjects/dlkt/example/result_local/akt_ME-ADA_our_setting_assist2009_save_ob.txt
--- example/train/atkt.py
+++ example/train/atkt.py
@@ -15,15 +15,15 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     # 数据集相关
     parser.add_argument("--setting_name", type=str, default="our_setting")
-    parser.add_argument("--dataset_name", type=str, default="edi2020-task34")
+    parser.add_argument("--dataset_name", type=str, default="statics2011")
     parser.add_argument("--data_type", type=str, default="single_concept",
                         choices=("multi_concept", "single_concept", "only_question"))
-    parser.add_argument("--train_file_name", type=str, default="edi2020-task34_train_fold_0.txt")
-    parser.add_argument("--valid_file_name", type=str, default="edi2020-task34_valid_fold_0.txt")
-    parser.add_argument("--test_file_name", type=str, default="edi2020-task34_test_fold_0.txt")
+    parser.add_argument("--train_file_name", type=str, default="statics2011_train_fold_0.txt")
+    parser.add_argument("--valid_file_name", type=str, default="statics2011_valid_fold_0.txt")
+    parser.add_argument("--test_file_name", type=str, default="statics2011_test_fold_0.txt")
     # 优化器相关参数选择
     parser.add_argument("--optimizer_type", type=str, default="adam", choices=("adam", "sgd"))
-    parser.add_argument("--weight_decay", type=float, default=0.000001)
+    parser.add_argument("--weight_decay", type=float, default=0)
     parser.add_argument("--momentum", type=float, default=0.9)
     # 训练策略
     parser.add_argument("--train_strategy", type=str, default="valid_test", choices=("valid_test", "no_valid"))
@@ -51,16 +51,16 @@ if __name__ == "__main__":
     parser.add_argument("--enable_clip_grad", type=str2bool, default=False)
     parser.add_argument("--grad_clipped", type=float, default=10.0)
     # 模型参数
-    parser.add_argument("--use_concept", type=str2bool, default=False)
-    parser.add_argument("--num_concept", type=int, default=53)
-    parser.add_argument("--num_question", type=int, default=948)
+    parser.add_argument("--use_concept", type=str2bool, default=True)
+    parser.add_argument("--num_concept", type=int, default=27)
+    parser.add_argument("--num_question", type=int, default=1223)
     parser.add_argument("--dim_concept", type=int, default=64)
     parser.add_argument("--dim_correct", type=int, default=64)
-    parser.add_argument("--dim_latent", type=int, default=256)
+    parser.add_argument("--dim_latent", type=int, default=64)
     parser.add_argument("--dim_attention", type=int, default=64)
     parser.add_argument("--dropout", type=float, default=0.1)
-    parser.add_argument("--epsilon", type=int, default=15)
-    parser.add_argument("--beta", type=float, default=2)
+    parser.add_argument("--epsilon", type=int, default=5)
+    parser.add_argument("--beta", type=float, default=0.1)
     # 其它
     parser.add_argument("--save_model", type=str2bool, default=False)
     parser.add_argument("--debug_mode", type=str2bool, default=False, help="if True, device=cpu, and set_detect_anomaly=True")

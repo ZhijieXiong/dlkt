003a5b13a9ef0b9ceddda38e03689abcc1ef9390
ZhijieXiong <18800118477@163.com>
2024-02-28 20:50:47 +0800

    update

--- example/script_local/run2.sh
+++ example/script_local/run2.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 
-bash F:/code/myProjects/dlkt/example/script_local/pykt_repo/atkt.sh
-bash F:/code/myProjects/dlkt/example/script_local/pykt_repo/atkt_que.sh
-
+bash F:/code/myProjects/dlkt/example/script_local/simple-kt/our_setting_bridge2algebra2006_save.sh
+bash F:/code/myProjects/dlkt/example/script_local/simple-kt/our_setting_ednet-kt1_save.sh
+bash F:/code/myProjects/dlkt/example/script_local/pykt_repo/simple-kt.sh
+bash F:/code/myProjects/dlkt/example/script_local/simple-kt/our_setting_assist2012_save.sh

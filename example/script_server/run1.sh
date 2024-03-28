d9a8bff6964d80320a3344e49cbd8d050f0e7982
ZhijieXiong <18800118477@163.com>
2024-03-29 03:16:57 +0800

    update

--- example/script_server/run1.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-
-bash /home/xiongzj/myProjects/KT/dlkt/example/script_server/lpkt/our_setting_xes3g5m_fold_0_ob.sh
-bash /home/xiongzj/myProjects/KT/dlkt/example/script_server/lpkt/our_setting_algebra2005_fold_0_ob.sh

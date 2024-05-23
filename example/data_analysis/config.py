3892050729eb7d2c9b8a2516b2e43c0f64b1671d
ZhijieXiong <18800118477@163.com>
2024-05-23 23:07:23 +0800

    update

---
+++ example/data_analysis/config.py
@@ -0,0 +1,13 @@
+import json
+import sys
+import os
+import inspect
+
+
+current_file_name = inspect.getfile(inspect.currentframe())
+current_dir = os.path.dirname(current_file_name)
+settings_path = os.path.join(current_dir, "../../settings.json")
+with open(settings_path, "r") as f:
+    settings = json.load(f)
+FILE_MANAGER_ROOT = settings["FILE_MANAGER_ROOT"]
+sys.path.append(settings["LIB_PATH"])

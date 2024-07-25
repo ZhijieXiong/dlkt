f27dc0ddfe6d6f609c175f362a000f776ef57b0a
ZhijieXiong <18800118477@163.com>
2024-07-25 10:07:54 +0800

    update

---
+++ example/train/config/sakt_config.py
@@ -0,0 +1,57 @@
+from ._config import *
+
+from lib.util.basic import *
+
+
+def sakt_general_config(local_params, global_params, global_objects):
+    global_params["models_config"] = {
+        "kt_model": {
+            "encoder_layer": {
+                "type": "SAKT",
+                "SAKT": {
+
+                }
+            }
+        }
+    }
+
+    # 配置模型参数
+    num_concept = local_params["num_concept"]
+    dim_emb = local_params["dim_emb"]
+    num_head = local_params["num_head"]
+    num_block = local_params["num_block"]
+    seq_len = local_params["seq_len"]
+    dropout = local_params["dropout"]
+
+    encode_config = global_params["models_config"]["kt_model"]["encoder_layer"]["SAKT"]
+    encode_config["num_concept"] = num_concept
+    encode_config["dim_emb"] = dim_emb
+    encode_config["num_head"] = num_head
+    encode_config["num_block"] = num_block
+    encode_config["seq_len"] = seq_len
+    encode_config["dropout"] = dropout
+
+    global_objects["logger"].info(
+        f"model params\n    "
+        f"num_concept: {num_concept}, dim_emb: {dim_emb}, num_block: {num_block}, num_head: {num_head}, "
+        f"dropout: {dropout}, seq_len: {seq_len}"
+    )
+
+    if local_params["save_model"]:
+        setting_name = local_params["setting_name"]
+        train_file_name = local_params["train_file_name"]
+
+        global_params["save_model_dir_name"] = (
+            f"{get_now_time().replace(' ', '@').replace(':', '-')}@@SAKT@@seed_{local_params['seed']}@@{setting_name}@@"
+            f"{train_file_name.replace('.txt', '')}")
+
+
+def sakt_config(local_params):
+    global_params = {}
+    global_objects = {}
+    general_config(local_params, global_params, global_objects)
+    sakt_general_config(local_params, global_params, global_objects)
+    if local_params["save_model"]:
+        save_params(global_params, global_objects)
+
+    return global_params, global_objects
--- lib/model/SAKT.py
+++ lib/model/SAKT.py
@@ -1,17 +1,110 @@
-from .BaseModel4CL import BaseModel4CL
-from .Module.KTEmbedLayer import KTEmbedLayer
-from .Module.PredictorLayer import PredictorLayer
-from .loss_util import *
-from .util import *
+import torch
+import torch.nn as nn
+from copy import deepcopy
 
 
-class Block(nn.Module):
-    def __init__(self, params):
+def position_encode(seq_len, device):
+    return torch.arange(seq_len).unsqueeze(0).to(device)
+
+
+def ut_mask(seq_len, device):
+    return torch.triu(torch.ones(seq_len, seq_len), diagonal=1).to(dtype=torch.bool).to(device)
+
+
+def get_clones(module, N):
+    return nn.ModuleList([deepcopy(module) for _ in range(N)])
+
+
+class transformer_FFN(nn.Module):
+    def __init__(self, emb_size, dropout) -> None:
         super().__init__()
+        self.emb_size = emb_size
+        self.dropout = dropout
+        self.FFN = nn.Sequential(
+            nn.Linear(self.emb_size, self.emb_size),
+            nn.ReLU(),
+            nn.Dropout(self.dropout),
+            nn.Linear(self.emb_size, self.emb_size),
+        )
 
+    def forward(self, in_fea):
+        return self.FFN(in_fea)
+
+
+class SAKT(nn.Module):
+    model_name = "SAKT"
+
+    def __init__(self, params, objects):
+        super(SAKT, self).__init__()
+        self.params = params
+        self.objects = objects
+
+        encoder_config = self.params["models_config"]["kt_model"]["encoder_layer"]["SAKT"]
+        num_concept = encoder_config["num_concept"]
+        dim_emb = encoder_config["dim_emb"]
+        dropout = encoder_config["dropout"]
+        num_block = encoder_config["num_block"]
+        seq_len = encoder_config["seq_len"]
+
+        self.embed_interaction = nn.Embedding(num_concept * 2, dim_emb)
+        self.embed_concept = nn.Embedding(num_concept, dim_emb)
+        self.embed_position = nn.Embedding(seq_len, dim_emb)
+
+        self.blocks = get_clones(Blocks(params), num_block)
+
+        self.dropout_layer = nn.Dropout(dropout)
+        self.out = nn.Linear(dim_emb, 1)
+
+    def forward(self, batch):
+        encoder_config = self.params["models_config"]["kt_model"]["encoder_layer"]["SAKT"]
+        num_concept = encoder_config["num_concept"]
+        num_block = encoder_config["num_block"]
+
+        concept_seq = batch["concept_seq"]
+        correct_seq = batch["correct_seq"]
+
+        # 历史的interaction
+        interaction_seq = concept_seq[:, :-1] + num_concept * correct_seq[:, :-1]
+        interaction_emb = self.embed_interaction(interaction_seq)
+        position_emb = self.embed_position(position_encode(interaction_emb.shape[1], self.params["device"]))
+        interaction_emb = interaction_emb + position_emb
+
+        # 未来的concept
+        concept_emb = self.embed_concept(concept_seq[:, 1:])
+        for i in range(num_block):
+            interaction_emb = self.blocks[i](concept_emb, interaction_emb, interaction_emb)
+
+        p = torch.sigmoid(self.out(self.dropout_layer(interaction_emb))).squeeze(-1)
+
+        return p
+
+    def get_predict_score(self, batch):
+        mask_bool_seq = torch.ne(batch["mask_seq"], 0)
+        predict_score = self.forward(batch)
+        predict_score = torch.masked_select(predict_score, mask_bool_seq[:, 1:])
+
+        return predict_score
+
+    def get_predict_loss(self, batch, loss_record=None):
+        mask_bool_seq = torch.ne(batch["mask_seq"], 0)
+
+        predict_score = self.get_predict_score(batch)
+        ground_truth = torch.masked_select(batch["correct_seq"][:, 1:], mask_bool_seq[:, 1:])
+        predict_loss = nn.functional.binary_cross_entropy(predict_score.double(), ground_truth.double())
+
+        if loss_record is not None:
+            num_sample = torch.sum(batch["mask_seq"][:, 1:]).item()
+            loss_record.add_loss("predict loss", predict_loss.detach().cpu().item() * num_sample, num_sample)
+
+        return predict_loss
+
+
+class Blocks(nn.Module):
+    def __init__(self, params) -> None:
+        super().__init__()
         self.params = params
 
-        encoder_config = params["models_config"]["kt_model"]["encoder_layer"]["qSAKT"]
+        encoder_config = self.params["models_config"]["kt_model"]["encoder_layer"]["SAKT"]
         dim_emb = encoder_config["dim_emb"]
         num_head = encoder_config["num_head"]
         dropout = encoder_config["dropout"]
@@ -20,42 +113,24 @@ class Block(nn.Module):
         self.attn_dropout = nn.Dropout(dropout)
         self.attn_layer_norm = nn.LayerNorm(dim_emb)
 
-        self.FFN = nn.Sequential(
-            nn.Linear(dim_emb, dim_emb),
-            nn.ReLU(),
-            nn.Dropout(dropout),
-            nn.Linear(dim_emb, dim_emb),
-            nn.Dropout(dropout)
-        )
+        self.FFN = transformer_FFN(dim_emb, dropout)
+        self.FFN_dropout = nn.Dropout(dropout)
         self.FFN_layer_norm = nn.LayerNorm(dim_emb)
 
-    def forward(self, q, k, v):
+    def forward(self, q=None, k=None, v=None):
         q, k, v = q.permute(1, 0, 2), k.permute(1, 0, 2), v.permute(1, 0, 2)
-        seq_len = k.shape[0]
-        causal_mask = torch.triu(torch.ones(seq_len, seq_len), diagonal=1).bool().to(self.params["device"])
-
-        # transformer default: attn -> drop -> skip -> norm
+        # attn -> drop -> skip -> norm
+        # transformer: attn -> drop -> skip -> norm transformer default
+        causal_mask = ut_mask(k.shape[0], self.params["device"])
         attn_emb, _ = self.attn(q, k, v, attn_mask=causal_mask)
+
         attn_emb = self.attn_dropout(attn_emb)
         attn_emb, q = attn_emb.permute(1, 0, 2), q.permute(1, 0, 2)
+
         attn_emb = self.attn_layer_norm(q + attn_emb)
 
         emb = self.FFN(attn_emb)
+        emb = self.FFN_dropout(emb)
         emb = self.FFN_layer_norm(attn_emb + emb)
 
         return emb
-
-
-class SAKT(nn.Module):
-    model_name = "SAKT"
-
-    def __init__(self, params, objects):
-        super(SAKT, self).__init__()
-
-        pass
-
-    def get_concept_emb(self):
-        return self.embed_concept.weight
-
-    def forward(self, batch):
-        pass

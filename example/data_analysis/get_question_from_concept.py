b245c6f33da3ea01c4661d0b2abecf56b43b46ea
ZhijieXiong <18800118477@163.com>
2024-10-10 09:21:03 +0800

    Stop tracking file but keep it locally

--- example/data_analysis/get_question_from_concept.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-from lib.util.parse import concept2question_from_Q
-
-Q_table_path = r"F:\code\myProjects\dlkt\lab\dataset_preprocessed/assist2012/Q_table_single_concept.npy"
-target_concept = 22
-Q_table = np.load(Q_table_path)
-concept2question = concept2question_from_Q(Q_table)
-print(concept2question[target_concept])
